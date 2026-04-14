83ebfe3cc92ccf5b1f9f97faed2b8402df738850
km1031kim@naver.com <km1031kim@naver.com>
2026-04-14 10:18:08 +0900

    jar, fat jar

--- src/main/java/hello/embed/EmbedTomcatSpringMain.java
+++ src/main/java/hello/embed/EmbedTomcatSpringMain.java
@@ -8,9 +8,9 @@ import org.apache.catalina.startup.Tomcat;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-public class EmbeddedTomcatSpringMain {
+public class EmbedTomcatSpringMain {
     public static void main(String[] args) throws LifecycleException {
-        System.out.println("EmbeddedTomcatSpringMain.main");
+        System.out.println("EmbedTomcatSpringMain.main");
         // 톰캣 설정
         Tomcat tomcat = new Tomcat();
         Connector connector = new Connector();
